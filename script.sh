db6c096f7dd12141161869568c11b587e987b66f
huberti1 <97759872+huberti1@users.noreply.github.com>
2023-01-27 19:37:41 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,62 +1,38 @@
 #!/bin/bash
 
-# This script displays a list of 10% of the elements in a specified directory.
-# Elements include both files and directories.
-# It accepts up to two arguments: src:... and dat:..., and their order is not important.
-# Argument src: specifies the directory and path, e.g. src:/home/Adam
-# Argument dat: specifies the sorting of elements:
-#    dat:oldest sorts by oldest
-#    dat:newest sorts by newest (default)
-# The script accepts the following combinations of arguments:
-#    src, dat
-#    src
-#    dat
-#    If no arguments are provided, it returns an error message.
-#    If the dat: argument is not provided, it defaults to sorting by newest.
-#    If the src: argument is not provided, it defaults to the current directory.
-# If the list has less than 10 elements, the script will display only one element.
+# Set default values for arguments
+src_dir="./"
+sort_order="newest"
 
-# Check for the presence of arguments
-if [ $# -eq 0 ]; then
-  echo "Error: No arguments provided."
-  exit 1
-fi
-
-# Initialize variables
-src_dir=`pwd` # Default to current directory
-dat_arg="newest" # Default to sorting by newest
-
-# Parse arguments
-for arg in "$@"
-do
+# Check for and parse arguments
+for arg in "$@"; do
   case $arg in
     src:*)
       src_dir="${arg#src:}"
       ;;
-    dat:oldest)
-      dat_arg="oldest"
-      ;;
-    dat:newest)
-      dat_arg="newest"
+    dat:*)
+      sort_order="${arg#dat:}"
       ;;
   esac
 done
 
-# Get the list of elements in the specified directory
-elements=`ls -U $src_dir`
+# Get list of files and directories in the specified directory (including subdirectories)
+file_list=$(find "$src_dir" -maxdepth 1 -type f -or -type d)
 
-# Sort the list of elements
-if [ $dat_arg == "oldest" ]; then
-  elements=`ls -Utr $src_dir`
+# Sort the list based on the specified order
+if [ "$sort_order" == "oldest" ]; then
+  file_list=$(echo "$file_list" | sort -n)
 else
-  elements=`ls -Ut $src_dir`
+  file_list=$(echo "$file_list" | sort -nr)
 fi
 
-# Get the number of elements
-num_elements=`echo "$elements" | wc -l`
+# Get the number of items in the list
+num_items=$(echo "$file_list" | wc -l)
 
-# Calculate the number of elements to display
-num_to_display=`echo $((num_elements / 10 + 1))`
-
-# Display the list of elements
-echo "$elements" | head -n $num_to_display
+# If there are less than 10 items, only display one item
+if [ "$num_items" -lt 10 ]; then
+  echo "$file_list" | head -n 1
+else
+  # Display the first 10% of items
+  echo "$file_list" | head -n $(($num_items/10))
+fi
