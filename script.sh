40a7eeb4bf34fe5ca7c9d6cee5af2cf2fb59ec2f
huberti1 <97759872+huberti1@users.noreply.github.com>
2023-01-27 20:41:24 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
-
-# Set default values for arguments
-src_dir="./"
-sort_order="newest"
-
-# Check for and parse arguments
+#Function to handle error messages
+handle_error(){
+echo "Error: $1"
+exit 1
+}
+#Default values for src and dat arguments
+src="./"
+dat="newest"
+#Check for arguments
+if [ $# -eq 0 ]; then
+handle_error "No arguments provided"
+else
 for arg in "$@"; do
-  case $arg in
-    src:*)
-      src_dir="${arg#src:}"
-      ;;
-    dat:*)
-      sort_order="${arg#dat:}"
-      ;;
-  esac
-done
-
-# Get list of files and directories in the specified directory (including subdirectories)
-file_list=$(find "$src_dir" -maxdepth 1 -type f -or -type d)
-
-# Sort the list based on the specified order
-if [ "$sort_order" == "oldest" ]; then
-  file_list=$(echo "$file_list" | sort -n)
+if [[ $arg == src:* ]]; then
+src="${arg:4}"
+elif [[ $arg == dat:* ]]; then
+dat="${arg:4}"
 else
-  file_list=$(echo "$file_list" | sort -nr)
+handle_error "Invalid argument: $arg"
 fi
-
-# Get the number of items in the list
-num_items=$(echo "$file_list" | wc -l)
-
-# If there are less than 10 items, only display one item
-if [ "$num_items" -lt 10 ]; then
-  echo "$file_list" | head -n 1
+done
+fi
+#Check if src directory exists
+if [ ! -d "$src" ]; then
+handle_error "Directory not found: $src"
+fi
+#Get the list of files and directories in the src directory
+list=$(find "$src" -type f -or -type d)
+#Check the number of elements in the list
+if [ $(echo "$list" | wc -l) -lt 10 ]; then
+echo "$list"
 else
-  # Display the first 10% of items
-  echo "$file_list" | head -n $(($num_items/10))
+# Sort the list by modification time
+if [ "$dat" == "newest" ]; then
+list=$(echo "$list" | xargs ls -lt | awk '{print $9}')
+elif [ "$dat" == "oldest" ]; then
+list=$(echo "$list" | xargs ls -ltr | awk '{print $9}')
+else
+handle_error "Invalid value for dat argument: $dat"
+fi
+# Get the first 10% of elements in the list
+count=$(echo "$list" | wc -l)
+count=$((count / 10))
+list=$(echo "$list" | head -n $count)
+echo "$list"
 fi
