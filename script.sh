2b9e7f34df7b1d6e6417e9f9cb0817f37e9bbc59
huberti1 <97759872+huberti1@users.noreply.github.com>
2023-01-28 12:26:58 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,55 +1,58 @@
 #!/bin/bash
 
-# the directory to display the elements from
+# define default values for src and dat variables
 src="./"
-
-# the order of elements to display
 dat="newest"
 
-# loop through arguments
+# loop through arguments passed to the script
 for arg in "$@"
 do
-    # check if argument is specifying the source directory
+    # check if arg starts with "src:"
     if [[ $arg == src:* ]]; then
-        src=${arg#src:}
+        # remove "src:" from the arg and assign the value to src variable
+        src=${arg:4}
     fi
-
-    # check if argument is specifying the sort order
+    # check if arg starts with "dat:"
     if [[ $arg == dat:* ]]; then
-        dat=${arg#dat:}
+        # remove "dat:" from the arg and assign the value to dat variable
+        dat=${arg:4}
     fi
 done
 
-# check if source directory exists
+# check if src directory exists
 if [ ! -d "$src" ]; then
-    echo "Error: The source directory does not exist."
+    echo "Error: $src is not a valid directory"
     exit 1
 fi
 
-# get the list of elements in the source directory
-elements=( $(find "$src" -maxdepth 1 -printf '%T+ %p\n' | sort -r) )
+# get the list of files and directories in the src directory
+files_and_dirs=($src/*)
 
-# check if there are less than 10 elements
-if [ ${#elements[@]} -lt 10 ]; then
-    for element in "${elements[@]}"
-    do
-        echo ${element#* }
-    done
-    exit 0
+# check if files_and_dirs array is not empty
+if [ ${#files_and_dirs[@]} -eq 0 ]; then
+    echo "Error: $src is empty"
+    exit 1
 fi
 
-# determine the number of elements to display
-num_elements=$(( ${#elements[@]} / 10 ))
-
-# check if sort order is oldest
-if [ "$dat" == "oldest" ]; then
-    # loop through the elements in reverse order
-    for (( i=${#elements[@]}-1; i >= ${#elements[@]}-$num_elements; i-- )); do
-        echo ${elements[i]#* }
-    done
+# sort the list of files and directories based on the value of dat variable
+if [ $dat == "oldest" ]; then
+    files_and_dirs=($(for f in "${files_and_dirs[@]}"; do stat -c "%Y %n" "$f"; done | sort -n | awk '{$1=""; print $0}'))
 else
-    # loop through the elements
-    for (( i=0; i < num_elements; i++ )); do
-        echo ${elements[i]#* }
-    done
+    files_and_dirs=($(for f in "${files_and_dirs[@]}"; do stat -c "%Y %n" "$f"; done | sort -nr | awk '{$1=""; print $0}'))
 fi
+
+# calculate the number of files and directories to display
+num_to_display=$(echo "(${#files_and_dirs[@]} * 0.1)+1" | bc | awk '{print int($1)}')
+
+# check if num_to_display is greater than the number of files and directories
+if [ $num_to_display -gt ${#files_and_dirs[@]} ]; then
+    num_to_display=${#files_and_dirs[@]}
+fi
+
+# display the first num_to_display files and directories
+for ((i=0; i<num_to_display; i++)); do
+    # check if file or directory exists
+    if [ -e "${files_and_dirs[i]}" ]; then
+        echo ${files_and_dirs[i]}
+    fi
+done
