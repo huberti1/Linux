07cf24aeda037b980fa17bd3f2c17cb61e5dc7bc
huberti1 <97759872+huberti1@users.noreply.github.com>
2023-01-28 11:35:03 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -1,47 +1,47 @@
 #!/bin/bash
-#Function to handle error messages
-handle_error(){
-echo "Error: $1"
-exit 1
-}
-#Default values for src and dat arguments
-src="./"
-dat="newest"
-#Check for arguments
+
+# Check for the presence of arguments
 if [ $# -eq 0 ]; then
-handle_error "No arguments provided"
-else
+    echo "Error: No arguments provided"
+    exit 1
+fi
+
+# Set default values for src and dat
+src=$(pwd)
+dat="newest"
+
+# Parse arguments
 for arg in "$@"; do
-if [[ $arg == src:* ]]; then
-src="${arg:4}"
-elif [[ $arg == dat:* ]]; then
-dat="${arg:4}"
-else
-handle_error "Invalid argument: $arg"
-fi
+    case $arg in
+        src:*) src=${arg#src:} ;;
+        dat:oldest) dat="oldest" ;;
+        dat:newest) dat="newest" ;;
+    esac
 done
-fi
-#Check if src directory exists
+
+# Check if the src directory exists
 if [ ! -d "$src" ]; then
-handle_error "Directory not found: $src"
+    echo "Error: $src is not a valid directory"
+    exit 1
 fi
-#Get the list of files and directories in the src directory
-list=$(find "$src" -type f -or -type d)
-#Check the number of elements in the list
-if [ $(echo "$list" | wc -l) -lt 10 ]; then
-echo "$list"
+
+# Get list of files and directories in the src directory
+files=$(find "$src" -maxdepth 1 -type f -or -type d)
+
+# Sort files based on dat argument
+if [ "$dat" == "oldest" ]; then
+    files=$(echo "$files" | sort -n)
 else
-# Sort the list by modification time
-if [ "$dat" == "newest" ]; then
-list=$(echo "$list" | xargs ls -lt | awk '{print $9}')
-elif [ "$dat" == "oldest" ]; then
-list=$(echo "$list" | xargs ls -ltr | awk '{print $9}')
-else
-handle_error "Invalid value for dat argument: $dat"
+    files=$(echo "$files" | sort -nr)
 fi
-# Get the first 10% of elements in the list
-count=$(echo "$list" | wc -l)
-count=$((count / 10))
-list=$(echo "$list" | head -n $count)
-echo "$list"
+
+# Take 10% of the files
+files_count=$(echo "$files" | wc -l)
+files_count=$((files_count/10))
+if [ $files_count -eq 0 ]; then
+    files_count=1
 fi
+files=$(echo "$files" | head -n "$files_count")
+
+# Print the result
+echo "$files"
