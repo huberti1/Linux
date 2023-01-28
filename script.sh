c819d926f880fe5d92c03a251f8dfb89475f73fe
huberti1 <97759872+huberti1@users.noreply.github.com>
2023-01-28 11:57:15 +0100

    Add files via upload

--- script.sh
+++ script.sh
@@ -26,7 +26,7 @@ if [ ! -d "$src" ]; then
 fi
 
 # Get list of files and directories in the src directory
-files=$(find "$src" -type f -or -type d | sed '1d')
+files=$(find "$src" -maxdepth 1 -type f -or -type d | sed '1d')
 
 # Sort files based on dat argument
 if [ "$dat" == "oldest" ]; then
